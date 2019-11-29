aad961f9d0026f2de618630b47685263913b2961
wonsunahn <54322243+wonsunahn@users.noreply.github.com>
2019-11-29 09:50:37 -0500

    Add files via upload

---
+++ src/StackSort.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Random;
+
+import com.gradescope.jh61b.grader.GradedTest;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+
+public class StackSort {
+
+	public static int[] doStackSort(int[] test) {
+		return new int[] {4,5,6,7,8};
+	}
+}
